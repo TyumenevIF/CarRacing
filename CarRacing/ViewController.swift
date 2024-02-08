4f79e90f6faee82f5c67ca0c8fa96c9ce2cf40c1
Ilyas Tyumenev <tumenoff@mail.ru>
2024-02-08 20:38:14 +0300

    Initial commit

---
+++ CarRacing/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CarRacing
+//
+//  Created by Ilyas Tyumenev on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
